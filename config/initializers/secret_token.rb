7054206a64559ee9a88dffbac80133db48408a63
ryotashiraishi <ryota.shiraishi.for.facebook@gmail.com>
2014-10-12 15:22:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MetroApp::Application.config.secret_key_base = '727f0d578e946cd58ed8718210b71f1de24382d4530cb1b6255c3b2e6dc68e61fc8bfb88cca9b91edc954b83a35190a2428a1fc3286f2dd63c7bdcf12732e596'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MetroApp::Application.config.session_store :cookie_store, key: '_metro_app_session'
