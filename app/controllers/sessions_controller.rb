cbbbe56fff220e3e9c044b8065afaf44efa3c080
ryotashiraishi <ryota.shiraishi.for.facebook@gmail.com>
2014-10-13 13:57:20 +0900

    ページ遷移先を変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
     session[:last_name] = auth['info']['last_name']
     session[:image_url] = auth['info']['image']
 
-    redirect_to root_path, notice: 'Authentication is completed.'
+    redirect_to settings_index_path, notice: 'Authentication is completed.'
   end
 
   def destroy
