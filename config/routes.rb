c755f0cd1e6f90cbea27f9406db7aa73498fc7a3
ryotashiraishi <ryota.shiraishi.for.facebook@gmail.com>
2014-10-13 13:23:28 +0900

    settingsコントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MetroApp::Application.routes.draw do
+  get "settings/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
